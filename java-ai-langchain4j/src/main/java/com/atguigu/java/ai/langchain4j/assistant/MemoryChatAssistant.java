f62c1d60c9640ba982ebe39ef2b90c8684706f89
tom123456-art <2470688428@qq.com>
2025-08-19 11:10:28 +0800

    测试提示词Prompt

--- java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/assistant/MemoryChatAssistant.java
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/assistant/MemoryChatAssistant.java
@@ -1,4 +1,6 @@
 package com.atguigu.java.ai.langchain4j.assistant;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 
@@ -8,5 +10,9 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
         chatMemory = "chatMemory"
 )
 public interface MemoryChatAssistant {
-    String chat(String message);
+    @UserMessage("你是我的好朋友,请用上海话回答问题,并且添加一些表情符号｡ {{message}}") //{{it}}表示这里唯一的参数的占位符
+    String chat(@V("message")String message);
 }
+
+
+
