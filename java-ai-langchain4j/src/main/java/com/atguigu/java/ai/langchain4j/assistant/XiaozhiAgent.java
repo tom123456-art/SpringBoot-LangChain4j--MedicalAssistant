2a9681808ad6b790b52196415885edd7239a0c74
tom123456-art <2470688428@qq.com>
2025-08-19 16:03:15 +0800

    创建和配置Tools，实现并测试预约相关业务逻辑

--- java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/assistant/XiaozhiAgent.java
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/assistant/XiaozhiAgent.java
@@ -7,7 +7,9 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 @AiService(
         wiringMode = EXPLICIT,
         chatModel = "qwenChatModel",
-        chatMemoryProvider = "chatMemoryProviderXiaozhi")
+        chatMemoryProvider = "chatMemoryProviderXiaozhi",
+        tools = "appointmentTools" //tools配置
+)
 public interface XiaozhiAgent {
     @SystemMessage(fromResource = "xiaozhi-prompt-template.txt")
     String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
